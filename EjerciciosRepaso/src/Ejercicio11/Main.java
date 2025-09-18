d8c82705361397117d27a6a3a791d16484648d68
Javito999 <patxixav9>
2025-09-18 18:24:29 +0200

    reparado el repo

---
+++ EjerciciosRepaso/src/Ejercicio11/Main.java
@@ -0,0 +1,21 @@
+package Ejercicio11;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		ArrayList<Persona>  listaPersonas = new ArrayList<>();
+		
+		
+		
+		
+		
+
+	}
+
+}
