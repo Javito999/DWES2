b7eb4fa878b103f06747855892b6fdd6dee22885
Javito999 <146422988+Javito999@users.noreply.github.com>
2025-09-23 10:57:04 +0200

    inicio ejercicio repaso BBDD

---
+++ EjercicioRepasoBBDD/src/Utils/DBUtils.java
@@ -0,0 +1,22 @@
+package Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtils {
+
+	public static Connection conexionBBDD() throws ClassNotFoundException, SQLException {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String servidor = "jdbc:mysql://localhost:3306/classicmodels"; // url de
+		// conexión
+		String username = "root"; // Usuario
+		String password = "PracticaRoot"; // La contraseña de la BBDD
+		Connection conexionBD = DriverManager.getConnection(servidor, username, password);
+
+		return conexionBD;
+
+	}
+
+}
---
+++ EjercicioRepasoBBDD/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module EjercicioRepasoBBDD {
+	requires java.sql;
+}
