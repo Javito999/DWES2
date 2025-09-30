c7a627e2a7e424fbf48a5973af236dfa5aa9ee85
Javito999 <patxixav9>
2025-09-30 17:41:42 +0200

    ejercicio hora casi terminado

--- HoraActual/src/main/java/hora/HoraActual.java
+++ HoraActual/src/main/java/hora/HoraActual.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalTime;
+
+import dimeHora.DimeHoraActual;
 
 /**
  * Servlet implementation class HoraActual
@@ -15,32 +18,38 @@ import java.io.PrintWriter;
 public class HoraActual extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public HoraActual() {
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.setContentType("text/html");
-		HoraActual horaLocal = new HoraActual();
-		
-		PrintWriter out = response.getWriter();
-		out.print("<html>>body>");
-		out.print("<h3> La hora local es: " + horaLocal + "</h3>");
-		out.print("</html></body>");
-		
+	public HoraActual() {
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+
+		DimeHoraActual hora = new DimeHoraActual();
+		LocalTime horaLocal = hora.dimeHoraActual();
+
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>");
+		out.print("<h3> La hora local es: " + horaLocal + "</h3>");
+		out.print("</html></body>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
