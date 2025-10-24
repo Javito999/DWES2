583f12142414fa194055616e9e944e9e43367f0b
Javito999 <146422988+Javito999@users.noreply.github.com>
2025-10-24 13:25:42 +0200

    modificacion fuerza servlet

--- Garcia1JavierFuerzaCartaServlets/src/main/java/com/javito/controladores/Controlador.java
+++ Garcia1JavierFuerzaCartaServlets/src/main/java/com/javito/controladores/Controlador.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 /**
  * Servlet implementation class Controlador
  */
-@WebServlet("/Controlador")
+@WebServlet("/fuerza")
 public class Controlador extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
